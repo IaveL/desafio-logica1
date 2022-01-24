d43d87f856c64205f2cd9e7191c3846a9f872f62
Igor Avelino <igoravel21@gmail.com>
2022-01-24 15:03:19 -0300

    uploading challenge

---
+++ script.js
@@ -0,0 +1,18 @@
+const name = "Igor";
+
+let number = 54;
+
+let currentYear = 2022 - number;
+
+console.log("Soma das variáveis acima: ", currentYear+number)
+
+console.log("Estou mexendo no console.log!")
+
+let quartaFeira = false;
+
+console.log(quartaFeira)
+
+console.log(typeof(quartaFeira));
+
+
+
